7567ec6aad0118390a2c5a71cc85aa09df352f88
Gersomsim <gesomhernandez@me.com>
2026-03-11 00:24:54 -0600

    Configuracion de scroll

--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { NgxNotify } from '@gersomsim/ngx-notify';
 import { ngxNotifyConfig } from '../../ngx-notify.config';
@@ -8,7 +8,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled' })),
     NgxNotify.provide(ngxNotifyConfig)
   ]
 };
